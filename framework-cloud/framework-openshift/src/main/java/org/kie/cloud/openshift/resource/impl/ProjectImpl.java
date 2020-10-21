9f73f57175b5f3b6de50bfd468965abe16383f35
Jakub Schwan <jschwan@redhat.com>
2020-10-21 09:29:51 +0200

    tmp skip project delete for debuging

--- framework-cloud/framework-openshift/src/main/java/org/kie/cloud/openshift/resource/impl/ProjectImpl.java
+++ framework-cloud/framework-openshift/src/main/java/org/kie/cloud/openshift/resource/impl/ProjectImpl.java
@@ -82,7 +82,7 @@ public class ProjectImpl implements Project {
 
     @Override
     public void delete() {
-        openShift.deleteProject();
+        //openShift.deleteProject();
     }
 
     @Override
