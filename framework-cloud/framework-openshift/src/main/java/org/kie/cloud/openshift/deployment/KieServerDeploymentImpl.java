32aee80690914d52dc9dcd14065423df992f2fde
Jakub Schwan <jschwan@redhat.com>
2020-11-26 11:01:49 +0100

    Increase time limit for rollout to finish

--- framework-cloud/framework-openshift/src/main/java/org/kie/cloud/openshift/deployment/KieServerDeploymentImpl.java
+++ framework-cloud/framework-openshift/src/main/java/org/kie/cloud/openshift/deployment/KieServerDeploymentImpl.java
@@ -145,7 +145,7 @@ public class KieServerDeploymentImpl extends OpenShiftDeployment implements KieS
     private void waitForRolloutFinish(String rolloutInProgressConfigMapName) {
         if (getOpenShift().getConfigMap(rolloutInProgressConfigMapName) != null) {
             Supplier<ConfigMap> getConfigMap = () -> getOpenShift().getConfigMap(rolloutInProgressConfigMapName);
-            new SimpleWaiter(() -> OpenShiftCaller.repeatableCall(getConfigMap) == null).timeout(TimeUnit.MINUTES, 5)
+            new SimpleWaiter(() -> OpenShiftCaller.repeatableCall(getConfigMap) == null).timeout(TimeUnit.MINUTES, 10)
                                                                                         .reason("Temporary rollout config map found, waiting for rollout to finish.")
                                                                                         .waitFor();
         }
